62f6831b39b225ef7f2ec506c5ee7b34608a4a05
Anushka <Anushka@123>
2023-04-19 19:49:26 +0530

    first commit

---
+++ src/main/java/mypkg1/MyClass.java
@@ -0,0 +1,14 @@
+package mypkg1;
+
+public class MyClass {
+	public void show()
+	{
+		System.out.println("hello DevOps welcome to Jenkins");
+	}
+
+	public static void main(String[] args) {
+		MyClass ob = new MyClass();
+		ob.show();
+	}
+
+}
